711f2ec60c59ebdce02095c2eef4048bdb56aea2
Sleepwalker <webmaster@teambd.net>
2026-04-06 11:30:47 +0200

    feat: Update PostgreSQL images in docker-compose and remove deprecated SQL files

--- sql/CharacterBoosts.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS webapi."CharacterBoosts" (
-    character_guid bigint NOT NULL,
-    boost_type text NOT NULL, -- 'xp_boost', 'resource_boost', 'reputation_boost'
-    modifier float NOT NULL,
-    expiration_date timestamp with time zone NOT NULL,
-    PRIMARY KEY (character_guid, boost_type)
-);
-
-CREATE INDEX IF NOT EXISTS idx_character_boosts_expiration ON webapi."CharacterBoosts" (expiration_date);
